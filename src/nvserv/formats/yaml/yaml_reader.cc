0b5a8cf2508fbed1f9f461cdc4cca4fa5bdbfbcd
lwdjohari <linggawasistha.djohari@outlook.com>
2024-12-08 13:57:01 +0700

    WIP on YamlReader impl

--- src/nvserv/formats/yaml/yaml_reader.cc
+++ src/nvserv/formats/yaml/yaml_reader.cc
@@ -1,40 +1,193 @@
 #include "nvserv/formats/yaml/yaml_reader.h"
 
+#include <nvm/types/type_utility.h>
+#include <nvserv/exceptions.h>
+
+#include <sstream>
+
 NVSERV_BEGIN_NAMESPACE(formats::yaml)
 
+YamlReader::YamlReader(const std::string& path) {
+  LoadFromFile(path);
+}
 
-   YamlReader::YamlReader(const std::string& path){
+YamlReader::YamlReader(const uint8_t* bytes, size_t size) {
+  if (!bytes) {
+    nvserv::NullReferenceException("Yaml bytes null reference exception");
+  }
+}
 
-   }
+YamlReader::~YamlReader() {
+  if (!file_handle_ && file_handle_->is_open()) {
+    file_handle_->close();
+  }
+}
 
-   YamlReader::YamlReader(const uint8_t* bytes, size_t size){
-
-
-   }
-
-   YamlReader::~YamlReader(){
-
-   }
-
-  const YamlNodePtr YamlReader::Root() const{
-    return nullptr;
+bool YamlReader::IsOk() const {
+  if (!root_) {
+    return false;
   }
 
-  std::vector<YamlNodePtr> YamlReader::Nodes(YamlNodePtr parent) const{
-    return std::vector<YamlNodePtr>();
+  return true;
+}
+
+const YamlNodePtr YamlReader::Root() const {
+  if (!root_) {
+    return __NR_RETURN_MOVE(std::make_shared<YamlNode>(this, nullptr, false));
   }
 
-  std::vector<std::string> YamlReader::Keys(YamlNodePtr parent_key) const{
-     return std::vector<std::string>();
+  return __NR_RETURN_MOVE(std::make_shared<YamlNode>(this, root_, true));
+}
+
+void YamlReader::CheckNodePreOpsThrowsExceptionOnUnmeet(
+    const YamlNodePtr node) const {
+  if (!node) {
+    throw nvserv::NullReferenceException("ParentNode null-reference");
   }
-  std::vector<std::string> YamlReader::Keys(const std::string& parent_key) const{
+
+  if (!node->Ok()) {
+    throw nvserv::RuntimeException(
+        "ParentNode not in Ok status, can not do operations");
+  }
+}
+
+std::vector<YamlNodePtr> YamlReader::Nodes(const YamlNodePtr parent) const {
+  std::vector<YamlNodePtr> result;
+  CheckNodePreOpsThrowsExceptionOnUnmeet(parent);
+
+  std::shared_ptr<ryml::NodeRef> node_ref = __GetNodeRef(*parent, this);
+
+  if (!node_ref) {
+    return result;
+  }
+
+  if (!IsNodeIsSeqOrMap(parent)) {
+    return result;
+  }
+
+  for (ryml::NodeRef child = node_ref->first_child(); !child.invalid();
+       child = child.next_sibling()) {
+    result.push_back(std::move(std::make_shared<YamlNode>(
+        this, std::move(std::make_shared<ryml::NodeRef>(std::move(child))),
+        true)));
+  }
+
+  return __NR_RETURN_MOVE(result);
+}
+
+std::vector<std::string> YamlReader::Keys(const YamlNodePtr parent) const {
+  CheckNodePreOpsThrowsExceptionOnUnmeet(parent);
+
+  std::shared_ptr<ryml::NodeRef> node_ref = __GetNodeRef(*parent, this);
+
+  if (!node_ref) {
     return std::vector<std::string>();
   }
 
- // protected:
+  return std::vector<std::string>();
+}
 
-  void YamlReader::LoadFromFile(const std::string& file_path) const{}
-  void YamlReader::LoadFromBytes(const char* data, std::size_t size) const {}
+std::vector<std::string> YamlReader::Keys(const std::string& parent_key) const {
+  return std::vector<std::string>();
+}
 
-  
+const std::string& YamlReader::Filepath() const {
+  return file_path_;
+};
+
+// protected:
+
+void YamlReader::LoadFromFile(const std::string& file_path) {
+  file_handle_ = std::make_shared<std::ifstream>(
+      file_path, std::ios::in | std::ios::binary);
+
+  if (!file_handle_->is_open()) {
+    nvserv::IoException("Failed to open yaml file on: " + file_path);
+  }
+
+  file_path_ = std::string(file_path);
+  Parse(*file_handle_);
+}
+
+void YamlReader::LoadFromBytes(const char* data, std::size_t size) {
+  if (!data || size == 0) {
+    throw nvm::InvalidArgException(
+        "Failed to load Yaml byte, data is empty or null.");
+  }
+
+  std::stringstream stream;
+  stream.write(data, static_cast<std::streamsize>(size));
+  Parse(stream);
+}
+
+
+
+bool YamlReader::IsNodeIsSeqOrMap(const YamlNodePtr parent) const {
+  auto node_ref = __GetNodeRef(*parent, this);
+  if (!node_ref)
+    return false;
+
+  if (!node_ref->is_map() && !node_ref->is_seq()) {
+    return false;
+  }
+
+  return true;
+}
+
+// YamlNode Impl
+
+YamlNode::YamlNode(const YamlReader* reader,
+                   std::shared_ptr<ryml::NodeRef> node, bool is_ok)
+                : reader_(reader),
+                  node_(node),
+                  is_ok_(is_ok),
+                  node_type_(GetNodeType(node)) {}
+
+YamlNode::~YamlNode() {}
+
+const YamlReader* YamlNode::Reader() const {
+  return reader_;
+}
+
+bool YamlNode::Ok() const {
+  return is_ok_;
+}
+
+YamlNodeType YamlNode::Type() const {
+  return node_type_;
+}
+
+YamlNodeType YamlNode::GetNodeType(std::shared_ptr<ryml::NodeRef> node) const {
+  if (!node)
+    return YamlNodeType::Invalid;
+
+  if (node->is_map()) {
+    return YamlNodeType::Map;
+  } else if (node->is_seq()) {
+    return YamlNodeType::Sequence;
+  } else if (node->is_val()) {
+    return YamlNodeType::Scalar;
+  } else if (node->invalid()) {
+    return YamlNodeType::Invalid;
+  } else {
+    return YamlNodeType::Unknown;
+  }
+}
+
+std::shared_ptr<ryml::NodeRef> __GetNodeRef(const YamlNode& node,
+                                            const YamlReader* reader) {
+  if (!reader) {
+    throw nvserv::NullReferenceException(
+        "YamlReader null reference while trying to __GetNodeRef to YamlNode");
+  }
+
+  if (reader != node.reader_) {
+    throw nvserv::RuntimeException(
+        "YamlNode is not own by YamlReader, can not "
+        "passing node that don't owned by YamlReader");
+  }
+
+  return node.node_;
+}
+
 NVSERV_END_NAMESPACE
--- src/nvserv/formats/yaml/yaml_reader.h
+++ src/nvserv/formats/yaml/yaml_reader.h
@@ -5,28 +5,42 @@
 #include <memory>
 #include <ryml.hpp>
 #include <ryml_std.hpp>
-#include <sstream>
-#include <stdexcept>
 #include <string>
 #include <vector>
-
+#include <nvm/macro.h>
+#include <nvserv/exceptions.h>
 #include "nvserv/global.h"
-
+#include <nvm/types/type_utility.h>
 NVSERV_BEGIN_NAMESPACE(formats::yaml)
+
+enum class YamlNodeType{
+  Unknown=0,
+  Map = 1,
+  Sequence = 2,
+  Scalar = 3,
+  Invalid = 4
+};
 
 class YamlReader;
 
-using YamlReaderPtr = std::shared_ptr<YamlReader>;
+class YamlNode {
+ public:
+  explicit YamlNode(const YamlReader* reader,
+                    std::shared_ptr<ryml::NodeRef> node, bool is_ok);
+  virtual ~YamlNode();
 
-class YamlNode{
-  public:
-    explicit YamlNode(const YamlReaderPtr reader, ryml::NodeRef&& node);
-    virtual ~YamlNode();
+  const YamlReader* Reader() const;
+  bool Ok() const;
+  YamlNodeType Type() const;
+ private:
+  const YamlReader* reader_;
+  std::shared_ptr<ryml::NodeRef> node_;
+  bool is_ok_;
+  YamlNodeType node_type_;
 
-    const YamlReaderPtr Reader() const;
-  private:
-    ryml::NodeRef node_;
+  YamlNodeType GetNodeType(std::shared_ptr<ryml::NodeRef> node) const;
 
+  friend std::shared_ptr<ryml::NodeRef> __GetNodeRef(const YamlNode& node, const YamlReader* reader);
 };
 
 using YamlNodePtr = std::shared_ptr<YamlNode>;
@@ -38,20 +52,66 @@ class YamlReader {
   virtual ~YamlReader();
 
   const YamlNodePtr Root() const;
-  std::vector<YamlNodePtr> Nodes(YamlNodePtr parent) const;
-  std::vector<std::string> Keys(YamlNodePtr parent_key) const;
+  std::vector<YamlNodePtr> Nodes(const YamlNodePtr parent) const;
+  std::vector<std::string> Keys(const YamlNodePtr parent) const;
   std::vector<std::string> Keys(const std::string& parent_key) const;
+
+  const std::string& Filepath() const;
+  bool IsOk() const;
 
  protected:
   std::shared_ptr<ryml::Tree> tree_;
-  ryml::NodeRef root_;
+  std::shared_ptr<ryml::NodeRef> root_;
 
-  void LoadFromFile(const std::string& file_path) const;
-  void LoadFromBytes(const char* data, std::size_t size) const;
+  void LoadFromFile(const std::string& file_path);
+  void LoadFromBytes(const char* data, std::size_t size);
 
-  template <typename StreamType>
-  void Parse(StreamType& stream) const;
+  template <typename TStreamType>
+  void Parse(TStreamType& stream);
+  
 
+ private:
+  std::shared_ptr<std::ifstream> file_handle_;
+  std::string file_path_;
+
+  void CheckNodePreOpsThrowsExceptionOnUnmeet(const YamlNodePtr node) const;
+
+  bool IsNodeIsSeqOrMap(const YamlNodePtr parent) const;
+  bool IsNodeIsSeqOrdMapOrScalar(const YamlNodePtr parent) const;
 };
+
+
+// Parse any stream based backup by stringstream
+// TODO: Dirty implementations on memory, next optimization add option to parsed
+// in arena or in place to able to handle large files
+template <typename TStreamType>
+void YamlReader::Parse(TStreamType& stream) {
+  // avoid double buffer if TStreamType is std::stringstream
+  if constexpr (nvm::types::utility::is_type_of_v<TStreamType,
+                                                  std::stringstream>) {
+    std::stringstream& buffer = stream;
+
+    try {
+      tree_ = std::make_shared<ryml::Tree>(
+          ryml::parse_in_arena(ryml::to_csubstr(buffer.str())));
+      root_ = std::make_shared<ryml::NodeRef>(tree_->rootref());
+    } catch (const std::exception& e) {
+      throw nvm::RuntimeException("Failed to parse YAML: " +
+                                  std::string(e.what()));
+    }
+  } else {
+    std::stringstream buffer;
+    buffer << stream.rdbuf();
+
+    try {
+      tree_ = std::make_shared<ryml::Tree>(
+          ryml::parse_in_arena(ryml::to_csubstr(buffer.str())));
+      root_ = std::make_shared<ryml::NodeRef>(tree_->rootref());
+    } catch (const std::exception& e) {
+      throw nvm::RuntimeException("Failed to parse YAML: " +
+                                  std::string(e.what()));
+    }
+  }
+}
 
 NVSERV_END_NAMESPACE
